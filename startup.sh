a722333403a84bcb862ac07e86edb82f9d51cc64
Farah <91642487+FarahAbdo@users.noreply.github.com>
2024-07-10 23:18:07 +0300

    Delete startup.sh

--- startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "#!/bin/bash" > startup.sh
-echo "python -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0 --server.enableCORS=false --server.enableXsrfProtection=false" >> startup.sh
